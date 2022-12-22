2559025994aefe48d160b72db943cce0ba943f0d
William <williamsoares058@gmail.com>
2022-12-21 23:37:46 -0300

    correção

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  API_URL: 'https://unialfa-hackaton-production.up.railway.app/',
+  API_URL: 'https://unialfa-hackaton-production.up.railway.app',
 };
